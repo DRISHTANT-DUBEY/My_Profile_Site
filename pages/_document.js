aa9f0078abf18ada4f1d7d7eed594834a40c8c51
Drishtant Dubey <drishtantkrishna@gmail.com>
2021-08-01 11:30:00 +0530

    Development Branch Testing

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+        </Head>
+        <body className="bg-black text-white">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
